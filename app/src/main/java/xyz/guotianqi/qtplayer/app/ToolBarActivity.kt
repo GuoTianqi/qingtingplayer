a7910960dd992eb7fe1cee158c602df3c9a9c9d3
guotianqi <guotianqi>
2018-05-30 17:38:37 +0800

    [Add] Add ToolBarActivity base class

---
+++ app/src/main/java/xyz/guotianqi/qtplayer/app/ToolBarActivity.kt
@@ -0,0 +1,28 @@
+package xyz.guotianqi.qtplayer.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import xyz.guotianqi.qtplayer.R
+
+open class ToolBarActivity: AppCompatActivity() {
+    protected lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.toolbar_activity)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.run {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
